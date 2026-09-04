42607fcf61bd2f53bdb6040d6ae660b2248d49ec
ArthurGeraldo <arthurgeraldo718@gmail.com>
2026-09-04 11:22:27 -0300

    feat: código funcionando para gerar QR Code

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import qrcode
 
-img = qrcode.make('Some data here')
+img = qrcode.make(input("Insira o valor para ser exibido junto ao QR Code: "))
 type(img)  # qrcode.image.pil.PilImage
-img.save("some_file.png")
+img.save(r".\qrcode_gen\qrcode.png")
+
+if img == True:
+    print("QR Code gerado com sucesso!")
+else:
+    print("Erro ao gerar QR Code!")
